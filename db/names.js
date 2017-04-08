ff608328548f8ac8792ac2bab4643c2e58047b34
Tony <terimyaki@users.noreply.github.com>
2017-04-08 13:59:27 -0400

    added general directory setup for db

---
+++ db/names.js
@@ -0,0 +1,18 @@
+// Edges
+const BRANCH = 'branch';
+const COMMIT = 'commit';
+const HEAD = 'head';
+
+// Vertices
+const CONTENT = 'content';
+const REPO = 'repo';
+const TREE = 'tree';
+
+module.exports = {
+  BRANCH,
+  COMMIT,
+  HEAD,
+  CONTENT,
+  REPO,
+  TREE,
+};
