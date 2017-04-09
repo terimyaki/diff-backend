a9f3cad3b84ec4324b409cb86eddbd408b35c477
Tony <terimyaki@users.noreply.github.com>
2017-04-09 17:20:03 -0400

    checking another way to fetch current db

--- db/vertex/repo.js
+++ db/vertex/repo.js
@@ -1,9 +1,7 @@
-const db = require('@arangodb').db;
 const { REPO } = require('../names');
-const repoVertex = db._collection(REPO);
-
-const list = () => repoVertex.all().toArray();
-const create = entry => repoVertex.save(entry);
+const getDbState = name => name ? require('@arangodb').db[name] : require('@arangodb').db;
+const list = () => getDb(REPO).all().toArray();
+const create = entry => getDb(REPO).save(entry);
 
 module.exports = {
   list,
