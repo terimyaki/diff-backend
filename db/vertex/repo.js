48c1f3a65db4aa33d8ac035acd79f43a0db65266
Tony <terimyaki@users.noreply.github.com>
2017-04-09 17:21:19 -0400

    syntax

--- db/vertex/repo.js
+++ db/vertex/repo.js
@@ -1,5 +1,5 @@
 const { REPO } = require('../names');
-const getDbState = name => name ? require('@arangodb').db[name] : require('@arangodb').db;
+const getDb = name => name ? require('@arangodb').db[name] : require('@arangodb').db;
 const list = () => getDb(REPO).all().toArray();
 const create = entry => getDb(REPO).save(entry);
 
